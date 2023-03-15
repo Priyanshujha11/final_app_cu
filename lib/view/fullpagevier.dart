428699b522e0e063be909367a48099db47ba6626
Aviral3108 <aviraldixitnam@gmail.com>
2023-03-15 23:12:25 +0530

    akash commit

---
+++ lib/view/fullpagevier.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class FullPageVideo extends StatefulWidget {
+  final VideoPlayerController videoPlayerController;
+  final String title;
+
+  const FullPageVideo({
+    Key? key,
+    required this.videoPlayerController,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  _FullPageVideoState createState() => _FullPageVideoState();
+}
+
+class _FullPageVideoState extends State<FullPageVideo> {
+  late VideoPlayerController _controller;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.videoPlayerController;
+    _controller.initialize().then((_) {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  void _togglePlayPause() {
+    setState(() {
+      if (_isPlaying) {
+        _controller.pause();
+        _isPlaying = false;
+      } else {
+        _controller.play();
+        _isPlaying = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+            onPressed: _togglePlayPause,
+          ),
+        ],
+      ),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+}
--- lib/view/gallery_page.dart
+++ lib/view/gallery_page.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_app_cu/view/full_page_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:video_player/video_player.dart';
+import 'package:final_app_cu/view/fullpagevier.dart';
 
 class GalleryPage extends StatelessWidget {
   const GalleryPage({super.key});
@@ -37,27 +39,53 @@ class GalleryPage extends StatelessWidget {
                   horizontal: 16.0,
                 ),
                 itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Get.to(
-                        FullPageImage(
-                          imgSrc: data[index]['url'].toString(),
-                          title: data[index]['desc'].toString(),
-                        ),
-                      );
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.grey[200],
-                        borderRadius: BorderRadius.circular(6.0),
-                        image: DecorationImage(
-                          image: NetworkImage(data[index]['type'] == 'picture'
-                              ? data[index]['url'].toString()
-                              : ''),
+                  if (data[index]['type'] == 'picture') {
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(
+                          FullPageImage(
+                            imgSrc: data[index]['url'].toString(),
+                            title: data[index]['desc'].toString(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(6.0),
+                          image: DecorationImage(
+                            image: NetworkImage(data[index]['url'].toString()),
+                          ),
                         ),
                       ),
-                    ),
-                  );
+                    );
+                  } else if (data[index]['type'] == 'video') {
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(
+                          FullPageVideo(
+                            videoPlayerController: VideoPlayerController.network(
+                              data[index]['url'].toString(),
+                            ),
+                            title: data[index]['title'].toString(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(6.0),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                              data[index]['thumbnail'].toString(),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else {
+                    return Container(); // handle other types of media
+                  }
                 },
               ),
             );
@@ -65,3 +93,4 @@ class GalleryPage extends StatelessWidget {
     );
   }
 }
+
